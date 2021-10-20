6599fb2bdedbc16dfedc78a25bb98bdef60981ff
Damien Diaz <contact.damiendiaz@gmail.com>
2021-10-20 10:02:30 +0200

    add file

--- public/index.php
+++ public/index.php
@@ -2,8 +2,15 @@
 
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
+require '../vendor/autoload.php';
+require 'products.php';
 
 
 $name = 'Wilder';
 
 echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('products.html.twig', ['products' => $products]);
+
+
+
+
